493811a35d8846ca0ab312339f916f0d9af74d0c
Holger Gruber <holger@gruber-frankfurt.de>
2016-02-09 11:56:03 +0100

    ENV/WORKDIR support, minor code changes

--- vzfactory.py
+++ vzfactory.py
@@ -10,63 +10,111 @@ import subprocess
 vid        = '1000'
 dockerfile = 'Dockerfile'
 ip         = '10.0.2.18'
+vzconf     = '/etc/vz/vz.conf'
+workdir    = '/'
+
 name       = os.path.dirname(os.path.realpath(dockerfile)).split('/')[-1].lower()
+environment= []
+maintainer = ''
+
+def vzroot():
+    root = re.compile('^VE_ROOT=(.*)\$.*')
+    for line in open(vzconf).readlines():
+        if root.match(line): return root.sub('\\1', line).strip()
+        else: return '/vz/root/'
+
+def vzabort(arg):
+    print arg
+    print 'Aborting'
+    sys.exit(0)
 
 def call(arg):
     if subprocess.call(arg) != 0:
         print 'Failed:'
-        print arg
-        print 'Aborting'
-        sys.exit(0)
+        vzabort(arg)
 
 def vzcreate(arg):
     call(['/usr/sbin/vzctl', 'create', vid, '--ostemplate', arg, '--ipadd', ip, '--name', name, '--hostname', name ])
     call(['/usr/sbin/vzctl', 'start', vid])
     # src directory where Dockerfile is located
     src = os.path.dirname(os.path.realpath(dockerfile))
-    dst = '/vz/root/' + vid + '/vztmp'
+    dst = vzroot() + vid + '/vztmp'
     call(['/usr/sbin/vzctl', 'exec2', vid, 'mkdir /vztmp'])
     call(['/bin/mount', '--bind', src, dst])
 
 def vzadd(arg):
-    lastarg = arg.split(' ')[-1]
-    call(['/usr/sbin/vzctl', 'exec2', vid, 'cd /vztmp; mkdir -p $(dirname '+lastarg+'); cp -r '+arg])
+    files = ''
+    try: a = json.loads(arg)
+    except:
+        lastarg = arg.split(' ')[-1]
+        if lastarg[0] != '/':
+            lastarg = workdir + lastarg
+        array = arg.split(' ')
+        for file in array[:-1]: files = files + file + ' '
+        files = files + lastarg
+    else:
+        lastarg = a[-1]
+        if lastarg[0] != '/':
+            lastarg = workdir + lastarg
+        for file in a[:-1]: files = files + '"' + file + '" '
+        files = files + '"' + lastarg + '"'
+    call(['/usr/sbin/vzctl', 'exec2', vid, 'cd /vztmp; mkdir -p $(dirname '+lastarg+'); cp -rv '+files])
 
 def vzexec(arg):
     call(['/usr/sbin/vzctl', 'exec2', vid, arg])
 
+def vzenv(arg):
+    element = arg.split(' ')
+    if '=' in element[0]:
+        for elem in element:
+            a = elem.split('=')
+            environment.append({a[0]: a[1]})
+    else:
+        if len(element) != 2:
+            vzabort('environment variable without value is not allowed')
+        environment.append({element[0]: element[1]})
+
+def vzdir(arg):
+    workdir = arg
+    if workdir[-1] != '/': workdir = workdir + '/'
+
+def vzmaintain(arg):
+    maintainer = arg
+
 def nop(arg):
     print '  --> NOT implemented'
+
 
 image_change = [ 'FROM', 'LABEL', 'ADD', 'COPY', 'RUN', 'ENV', 'ENTRYPOINT', 'CMD' ]
 functions = {
     'FROM':         vzcreate,
-    'MAINTAINER':   nop,
+    'MAINTAINER':   vzmaintain,
     'RUN':          vzexec,
     'CMD':          nop,
     'LABEL':        nop,
     'EXPOSE':       nop,
-    'ENV':          nop,
+    'ENV':          vzenv,
     'ADD':          vzadd,
     'COPY':         vzadd,
     'ENTRYPOINT':   nop,
     'VOLUME':       nop,
     'USER':         nop,
-    'WORKDIR':      nop,
+    'WORKDIR':      vzdir,
     'ARG':          nop,
     'ONBUILD':      nop,
     'STOPSIGNAL':   nop
 }
 
 def run(commands):
+    step = 1
     for command in commands:
         if command['command'] not in functions:
-            print command['command'], command['arguments'], 'NOT LISTED'
-            continue
+            vzabort('Error: unknown command ' + command['command'])
         cmd = functions[command['command']]
         arg = command['arguments']
-        print command['command'], arg
+        print 'Step ' + str(step) + ': ' + command['command'] + ' ' + arg
         cmd(arg)
+        step = step + 1
 
 def parse_dockerfile(dockerfile):
     comment = re.compile('^(#.*)$')
